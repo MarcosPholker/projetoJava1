c689ee3ecd85412bb1b03654a6bfb8cdd98b4105
toinh <toinh@DESKTOP-RL9L24R>
2023-05-15 13:40:27 -0300

    adicionando novos atributos

--- primeiroProgramaJava/src/curso/java/marcos/Pessoa.java
+++ primeiroProgramaJava/src/curso/java/marcos/Pessoa.java
@@ -3,18 +3,39 @@ package curso.java.marcos;
 public class Pessoa {
 	private String name;
 	private String sobreNome;
+	private Integer idade;
+	private String cidade;
 	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public String getSobreNome() {
 		return sobreNome;
 	}
+	
 	public void setSobreNome(String sobreNome) {
 		this.sobreNome = sobreNome;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
 	}
 	
 }
--- primeiroProgramaJava/src/cursoJava/executavel/PrimeiraClasseJava.java
+++ primeiroProgramaJava/src/cursoJava/executavel/PrimeiraClasseJava.java
@@ -4,6 +4,6 @@ public class PrimeiraClasseJava {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		Integer x = 10;
-		System.out.println("valor de x: " + x);
+		System.out.println("valor é de x: " + x);
 	}
 }
