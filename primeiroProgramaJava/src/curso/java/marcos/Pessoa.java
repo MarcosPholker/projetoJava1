671c933af5c986eb069b964f3707fd911150e901
toinh <toinh@DESKTOP-RL9L24R>
2023-05-15 13:43:21 -0300

    Revert "atributo estado"
    This reverts commit 3c8cba2365335c7093f6f29c140221df9c92187b.

--- primeiroProgramaJava/src/curso/java/marcos/Pessoa.java
+++ primeiroProgramaJava/src/curso/java/marcos/Pessoa.java
@@ -5,7 +5,6 @@ public class Pessoa {
 	private String sobreNome;
 	private Integer idade;
 	private String cidade;
-	private String estado;
 	
 	public String getName() {
 		return name;
@@ -37,14 +36,6 @@ public class Pessoa {
 
 	public void setCidade(String cidade) {
 		this.cidade = cidade;
-	}
-
-	public String getEstado() {
-		return estado;
-	}
-
-	public void setEstado(String estado) {
-		this.estado = estado;
 	}
 	
 }
