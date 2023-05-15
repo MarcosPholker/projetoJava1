2ec05521ec7a346e6f8716a8a17ad1cc72aaaffc
toinh <toinh@DESKTOP-RL9L24R>
2023-05-15 13:33:14 -0300

    alteração e criação da classe pessoa

---
+++ primeiroProgramaJava/src/curso/java/marcos/Pessoa.java
@@ -0,0 +1,20 @@
+package curso.java.marcos;
+
+public class Pessoa {
+	private String name;
+	private String sobreNome;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSobreNome() {
+		return sobreNome;
+	}
+	public void setSobreNome(String sobreNome) {
+		this.sobreNome = sobreNome;
+	}
+	
+}
