28035a9fe1546c75c279204ff29ac20b69b0c974
toinh <toinh@DESKTOP-RL9L24R>
2023-05-15 13:15:24 -0300

    Criação do projeto

---
+++ primeiroProgramaJava/src/cursoJava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,9 @@
+package cursoJava.executavel;
+
+public class PrimeiraClasseJava {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		Integer x = 10;
+		System.out.println("valor de x: " + x);
+	}
+}
